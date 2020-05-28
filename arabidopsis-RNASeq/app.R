057e2ca69e5ad63ab45075e416a634922719e7c7
Bryce Askey <bryceaskey@gmail.com>
2020-05-28 09:58:50 -0400

    Added data for all 6 genotypes, and functionality to deselect genes

--- arabidopsis-RNASeq/app.R
+++ arabidopsis-RNASeq/app.R
@@ -24,19 +24,28 @@ ui <- fluidPage(
   fluidRow(
     div(style="margin-top:0px", 
       column(3, 
-        h2("RNAseq data viewer"),
+        tags$h2("RNAseq data viewer"),
         wellPanel(
-          p("Tool for interactive viewing and subsetting of Arabidopsis RNAseq data. 
-            Click on genes in the lower table to add them to the plot.
-            Filter and search columns by using the table header.")
+          tags$p("Tool for interactive viewing and subsetting of Arabidopsis RNAseq data."),
+          tags$br(),
+          tags$p(tags$strong("To use:")),
+          tags$ol(
+            tags$li("Search and filter for genes in the lower table with the header row of the
+                    table."),
+            tags$li("Select genes to plot from the lower table by clicking on them."),
+            tags$li("Genes being plotted will appear in the table to the right of the plot 
+                    in the order that they were selected."),
+            tags$li("To remove genes from the plot, click on them in the right table")
+               
+          )
         )
       )
     ),
-    div(style="margin-top:15px",
-      column(5,
-        plotOutput("genePlot", height="300px")
+    tags$div(style="margin-top:60px; margin-bottom:470px",
+      column(6,
+        plotOutput("genePlot", height="390px")
       ),
-      column(4,
+      column(3,
         DTOutput("selectedGenes")
       )
     ),
@@ -46,44 +55,111 @@ ui <- fluidPage(
   )
 )
 
+if (!exists("default_search_columns")) default_search_columns <- NULL
+
 server <- function(input, output, session){
+  RD <- reactiveValues(allData=RNAseq, selectedData=NULL)
   
-  output$expressionData <- renderDT(RNAseq, 
-    server=TRUE, filter="top", selection="multiple", class="compact", 
+  output$expressionData <- renderDT(RD$allData[, c(1:3, 4, 6, 8, 10, 12, 14)], 
+    server=TRUE, filter="top", selection="none", class="compact hover",
+    colnames=c("Gene_ID", "Gene_name", "Gene_family", "WT_FPKM", "Ref5_FPKM", "Ref2_FPKM", "Med5_FPKM", "Ref5Med5_FPKM", "Ref2Med5_FPKM"),
     options=list(
-      sDom="<'top'>lrt<'bottom'>ip",
+      sDom="<'top'>rt<'bottom'>ip",
       autoWidth=TRUE,
       scrollX=TRUE,
-      columnDefs=list(list(width="10%", targets=c(4, 5, 6, 7))),
-      pageLength=10,
-      lengthMenu=c(10, 15, 20, 25))
+      columnDefs=list(list(width="7.5%", targets=c(4, 5, 6, 7, 8, 9)), list(width="15%", targets=c(1, 2))),
+      pageLength=15,
+      searchCols=default_search_columns,
+      stateSave=FALSE)
   )
   
-  output$selectedGenes <- renderDT(RNAseq[input$expressionData_rows_selected, c(1, 2, 3)],
-    class="compact", selection="none",
+  proxy <- dataTableProxy("expressionData")
+  
+  observeEvent(input$expressionData_cell_clicked, {
+    rowClicked <- input$expressionData_cell_clicked$row
+    if(is.null(rowClicked)){
+      return()
+    }else{
+      RD$selectedData <- rbind(RD$selectedData, RD$allData[rowClicked, ])
+      RD$allData <- RD$allData[-rowClicked, ]
+      
+      isolate({
+        default_search_columns <- c("", input$expressionData_search_columns)
+        proxy %>% updateSearch(keywords=list(columns=default_search_columns))
+      })
+    }
+  })
+
+  output$selectedGenes <- renderDT(RD$selectedData[, c(1, 2, 3)],
+    server=TRUE, class="compact hover", selection="none",
+    colnames=c("Gene_ID", "Gene_name", "Gene_family"),
     options=list(
+      ordering=FALSE,
+      paging=FALSE,
       dom="rti",
       autoWidth=TRUE,
-      scrollY="250px"
+      scrollY="320px",
+      searchCols=default_search_columns,
+      stateSave=FALSE
     )
   )
   
-  output$genePlot <- renderPlot({
-    if(length(input$expressionData_rows_selected)>0){
-      subsetData <- RNAseq[input$expressionData_rows_selected, ]
-      WT_data <- subsetData[ , 1:5]
-      WT_data$genotype <- "WT"
-      colnames(WT_data)[4:5] <- c("mean_FPKM", "se_FPKM")
-      Ref5_data <- subsetData[ , c(1:3, 6:7)]
-      Ref5_data$genotype <- "Ref5"
-      colnames(Ref5_data)[4:5] <- c("mean_FPKM", "se_FPKM")
-      subsetData <- rbind(WT_data, Ref5_data)
+  observeEvent(input$selectedGenes_cell_clicked, {
+    rowClicked <- input$selectedGenes_cell_clicked$row
+    if(is.null(rowClicked)){
+      return()
+    }else{
+      RD$allData <- rbind(RD$allData, RD$selectedData[rowClicked, ])
+      RD$selectedData <- RD$selectedData[-rowClicked, ]
       
-      ggplot(data=subsetData, mapping=aes(x=gene_id, y=mean_FPKM, fill=genotype)) +
+      isolate({
+        default_search_columns <- c("", input$expressionData_search_columns)
+        proxy %>% updateSearch(keywords=list(columns=default_search_columns))
+      })
+    }
+  })
+
+  output$genePlot <- renderPlot({
+    if(!is.null(RD$selectedData)){
+      WT_data <- RD$selectedData[ , 1:5]
+      WT_data$Genotype <- "WT"
+      colnames(WT_data)[4:5] <- c("FPKM", "stdError")
+      
+      Ref5_data <- RD$selectedData[ , c(1:3, 6:7)]
+      Ref5_data$Genotype <- "Ref5"
+      colnames(Ref5_data)[4:5] <- c("FPKM", "stdError")
+      
+      Ref2_data <- RD$selectedData[ , c(1:3, 8:9)]
+      Ref2_data$Genotype <- "Ref2"
+      colnames(Ref2_data)[4:5] <- c("FPKM", "stdError")
+      
+      Med5_data <- RD$selectedData[ , c(1:3, 10:11)]
+      Med5_data$Genotype <- "Med5"
+      colnames(Med5_data)[4:5] <- c("FPKM", "stdError")
+      
+      Ref5Med5_data <- RD$selectedData[ , c(1:3, 12:13)]
+      Ref5Med5_data$Genotype <- "Ref5Med5"
+      colnames(Ref5Med5_data)[4:5] <- c("FPKM", "stdError")
+      
+      Ref2Med5_data <- RD$selectedData[ , c(1:3, 14:15)]
+      Ref2Med5_data$Genotype <- "Ref2Med5"
+      colnames(Ref2Med5_data)[4:5] <- c("FPKM", "stdError")
+      
+      plotData <- rbind(WT_data, Ref5_data, Ref2_data, Med5_data, Ref5Med5_data, Ref2Med5_data)
+      colnames(plotData)[1:3] <- c("Gene_ID", "Gene_name", "Gene_family")
+      plotData$Genotype <- factor(as.character(plotData$Genotype), levels=c("WT", "Ref5", "Ref2", "Med5", "Ref5Med5", "Ref2Med5"))
+      plotData$Gene_ID <- factor(as.character(plotData$Gene_ID), levels=RD$selectedData$gene_id)
+      
+      ggplot(data=plotData, mapping=aes(x=Gene_ID, y=FPKM, fill=Genotype)) +
         geom_col(position=position_dodge()) +
-        geom_errorbar(mapping=aes(ymin=mean_FPKM-se_FPKM, ymax=mean_FPKM+se_FPKM), color="black",
+        geom_errorbar(mapping=aes(ymin=FPKM-stdError, ymax=FPKM+stdError), color="black",
                       width=0.2, position=position_dodge(0.9)) +
-        theme(legend.position="right")
+        theme_bw() +
+        theme(axis.text=element_text(size=12),
+              axis.title=element_text(size=14),
+              legend.text=element_text(size=12),
+              legend.title=element_text(size=14),
+              legend.position="right")
     }else{
       return(NA)
     }
--- arabidopsis-RNASeq/loadData.R
+++ arabidopsis-RNASeq/loadData.R
@@ -1,9 +1,9 @@
 library(stringr)
 
 # Script to load data from .csv file and save as a .rda file
-setwd("C:/Users/bca08_000/Documents/interactive-data/arabidopsis-RNAseq")
+setwd("C:/Users/Bryce/Documents/interactive-data/arabidopsis-RNAseq")
 
-RNAseq_raw <- read.csv("data/RNAseq_REF5_BB.csv")[ , 2:11]
+RNAseq_raw <- read.csv("data/RNAseq_REF5_BB.csv")[ , 2:25]
 geneFamilies <- read.csv("data/gene_families_sep_20_09_update.csv")
 geneFamilies$Genomic_Locus_Tag <- str_to_upper(geneFamilies$Genomic_Locus_Tag)
 colnames(geneFamilies)[1] <- "Gene_Family"
@@ -26,13 +26,35 @@ WT_mean_FPKM <- vector(mode="numeric", length=nrow(RNAseq_raw))
 WT_se_FPKM <- vector(mode="numeric", length=nrow(RNAseq_raw))
 Ref5_mean_FPKM <- vector(mode="numeric", length=nrow(RNAseq_raw))
 Ref5_se_FPKM <- vector(mode="numeric", length=nrow(RNAseq_raw))
+Ref2_mean_FPKM <- vector(mode="numeric", length=nrow(RNAseq_raw))
+Ref2_se_FPKM <- vector(mode="numeric", length=nrow(RNAseq_raw))
+Med5_mean_FPKM <- vector(mode="numeric", length=nrow(RNAseq_raw))
+Med5_se_FPKM <- vector(mode="numeric", length=nrow(RNAseq_raw))
+Ref5Med5_mean_FPKM <- vector(mode="numeric", length=nrow(RNAseq_raw))
+Ref5Med5_se_FPKM <- vector(mode="numeric", length=nrow(RNAseq_raw))
+Ref2Med5_mean_FPKM <- vector(mode="numeric", length=nrow(RNAseq_raw))
+Ref2Med5_se_FPKM <- vector(mode="numeric", length=nrow(RNAseq_raw))
 
 for(i in 1:nrow(RNAseq_raw)){
   gene_family[i] <- getGeneFamily(RNAseq_raw$gene_id[i])
+  
   WT_mean_FPKM[i] <- mean(c(RNAseq_raw$X011351_WT.1_FPKM[i], RNAseq_raw$X011352_WT.2_FPKM[i], RNAseq_raw$X011353_WT.3_FPKM[i]))
   WT_se_FPKM[i] <- sd(c(RNAseq_raw$X011351_WT.1_FPKM[i], RNAseq_raw$X011352_WT.2_FPKM[i], RNAseq_raw$X011353_WT.3_FPKM[i]))/sqrt(3)
+  
   Ref5_mean_FPKM[i] <- mean(c(RNAseq_raw$X011354_Ref5.1_FPKM[i], RNAseq_raw$X011355_Ref5.2_FPKM[i], RNAseq_raw$X011356_Ref5.3_FPKM[i]))
   Ref5_se_FPKM[i] <- sd(c(RNAseq_raw$X011354_Ref5.1_FPKM[i], RNAseq_raw$X011355_Ref5.2_FPKM[i], RNAseq_raw$X011356_Ref5.3_FPKM[i]))/sqrt(3)
+  
+  Ref2_mean_FPKM[i] <- mean(c(RNAseq_raw$X011357_Ref2.1_FPKM[i], RNAseq_raw$X011358_Ref2.2_FPKM[i], RNAseq_raw$X011359_Ref2.3_FPKM[i]))
+  Ref2_se_FPKM[i] <- sd(c(RNAseq_raw$X011357_Ref2.1_FPKM[i], RNAseq_raw$X011358_Ref2.2_FPKM[i], RNAseq_raw$X011359_Ref2.3_FPKM[i]))/sqrt(3)
+  
+  Med5_mean_FPKM[i] <- mean(c(RNAseq_raw$X011360_Med5.1_FPKM[i], RNAseq_raw$X011361_Med5.2_FPKM[i], RNAseq_raw$X011362_Med5.3_FPKM[i]))
+  Med5_se_FPKM[i] <- sd(c(RNAseq_raw$X011360_Med5.1_FPKM[i], RNAseq_raw$X011361_Med5.2_FPKM[i], RNAseq_raw$X011362_Med5.3_FPKM[i]))/sqrt(3)
+  
+  Ref5Med5_mean_FPKM[i] <- mean(c(RNAseq_raw$X011363_Ref5Med5.1_FPKM[i], RNAseq_raw$X011364_Ref5Med5.2_FPKM[i], RNAseq_raw$X011365_Ref5Med5.3_FPKM[i]))
+  Ref5Med5_se_FPKM[i] <- sd(c(RNAseq_raw$X011363_Ref5Med5.1_FPKM[i], RNAseq_raw$X011364_Ref5Med5.2_FPKM[i], RNAseq_raw$X011365_Ref5Med5.3_FPKM[i]))/sqrt(3)
+  
+  Ref2Med5_mean_FPKM[i] <- mean(c(RNAseq_raw$X011366_Ref2Med5.1_FPKM[i], RNAseq_raw$X011367_Ref2Med5.2_FPKM[i], RNAseq_raw$X011368_Ref2Med5.3_FPKM[i]))
+  Ref2Med5_se_FPKM[i] <- sd(c(RNAseq_raw$X011366_Ref2Med5.1_FPKM[i], RNAseq_raw$X011367_Ref2Med5.2_FPKM[i], RNAseq_raw$X011368_Ref2Med5.3_FPKM[i]))/sqrt(3)
 }
 
 set_dec_places <- function(num, dec_places){
@@ -47,10 +69,24 @@ RNAseq <- data.frame(
   gene_id=RNAseq_raw$gene_id,
   gene_name=RNAseq_raw$gene_short_name,
   gene_family=factor(gene_family),
+  
   WT_mean_FPKM=sapply(WT_mean_FPKM, set_dec_places, dec_places=3),
   WT_se_FPKM=sapply(WT_se_FPKM, set_dec_places, dec_places=3),
+  
   Ref5_mean_FPKM=sapply(Ref5_mean_FPKM, set_dec_places, dec_places=3),
-  Ref5_se_FPKM=sapply(Ref5_se_FPKM, set_dec_places, dec_places=3)
+  Ref5_se_FPKM=sapply(Ref5_se_FPKM, set_dec_places, dec_places=3),
+  
+  Ref2_mean_FPKM=sapply(Ref2_mean_FPKM, set_dec_places, dec_places=3),
+  Ref2_se_FPKM=sapply(Ref2_se_FPKM, set_dec_places, dec_places=3),
+  
+  Med5_mean_FPKM=sapply(Med5_mean_FPKM, set_dec_places, dec_places=3),
+  Med5_se_FPKM=sapply(Med5_se_FPKM, set_dec_places, dec_places=3),
+  
+  Ref5Med5_mean_FPKM=sapply(Ref5Med5_mean_FPKM, set_dec_places, dec_places=3),
+  Ref5Med5_se_FPKM=sapply(Ref5Med5_se_FPKM, set_dec_places, dec_places=3),
+  
+  Ref2Med5_mean_FPKM=sapply(Ref2Med5_mean_FPKM, set_dec_places, dec_places=3),
+  Ref2Med5_se_FPKM=sapply(Ref2Med5_se_FPKM, set_dec_places, dec_places=3)
 )
 
 save(RNAseq, file="data/RNAseq.rda")
