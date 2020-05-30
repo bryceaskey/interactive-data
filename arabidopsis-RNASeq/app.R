6e0dabfb94f6f91b5adae697137f59482495bcd1
Bryce Askey <bryceaskey@gmail.com>
2020-05-30 12:22:28 -0400

    Added functionality for filtering genotypes and exporting data

--- arabidopsis-RNASeq/app.R
+++ arabidopsis-RNASeq/app.R
@@ -2,7 +2,7 @@
 # Note: before running, working directory must be set to parent directory of app.R
 
 # Load necessary libraries, functions, and data ----
-packageList <- c("shiny", "shinyjs", "shinyWidgets", "dplyr", "DT", "ggplot2")
+packageList <- c("shiny", "shinyjs", "dplyr", "DT", "ggplot2")
 newPackages <- packageList[!(packageList %in% installed.packages()[,"Package"])]
 #if(length(newPackages) > 0){
 #  install.packages(newPackages)
@@ -17,38 +17,46 @@ library(ggplot2)
 source("helpers.R")
 
 load("data/RNAseq.rda")
+colnames(RNAseq) <- c("Gene_ID", "Gene_name", "Gene_family", "WT_FPKM", "WT_FPKM_SE", 
+  "Ref5_FPKM", "Ref5_FPKM_SE", "Ref2_FPKM", "Ref2_FPKM_SE", "Med5_FPKM", "Med5_FPKM_SE",
+  "Ref5Med5_FPKM", "Ref5Med5_FPKM_SE", "Ref2Med5_FPKM", "Ref2Med5_FPKM_SE")
 
 # Define UI ----
 ui <- fluidPage(
   useShinyjs(),
+  titlePanel("Arabidopsis RNAseq data viewer"),
   fluidRow(
-    div(style="margin-top:0px", 
-      column(3, 
-        tags$h2("RNAseq data viewer"),
-        wellPanel(
-          tags$p("Tool for interactive viewing and subsetting of Arabidopsis RNAseq data."),
-          tags$br(),
-          tags$p(tags$strong("To use:")),
-          tags$ol(
-            tags$li("Search and filter for genes in the lower table with the header row of the
-                    table."),
-            tags$li("Select genes to plot from the lower table by clicking on them."),
-            tags$li("Genes being plotted will appear in the table to the right of the plot 
-                    in the order that they were selected."),
-            tags$li("To remove genes from the plot, click on them in the right table")
-               
-          )
+    column(3, 
+      wellPanel(
+        tags$p("Tool for interactive subsetting of Arabidopsis RNAseq data."),
+        tags$p(tags$strong("To use:")),
+        tags$ol(
+          tags$li("Search and filter for genes in the lower table with the header row of the
+                  table."),
+          tags$li("Select genes to plot from the lower table by clicking on them."),
+          tags$li("Genes being plotted will appear in the table to the right of the plot 
+                  in the order that they were selected."),
+          tags$li("To remove genes from the plot, click on them in the right table")
         )
-      )
-    ),
-    tags$div(style="margin-top:60px; margin-bottom:470px",
-      column(6,
-        plotOutput("genePlot", height="390px")
       ),
-      column(3,
-        DTOutput("selectedGenes")
-      )
+      checkboxGroupInput("selectedGenotypes",
+        label=tags$p(tags$strong("Select genotypes to plot:")),
+        inline=FALSE,
+        choices=list("WT"="WT", "Ref5"="Ref5", "Ref2"="Ref2", "Med5"="Med5", "Ref5Med5"="Ref5Med5", "Ref2Med5"="Ref2Med5"),
+        selected=c("WT", "Ref5", "Ref2", "Med5", "Ref5Med5", "Ref2Med5"))
     ),
+    column(6,
+      plotOutput("genePlot", height="500px")
+    ),
+    column(3,
+      div(style="float:right; margin-bottom:20px",
+        downloadButton(style="display:inline; margin-right: 10px", "downloadGraph_pdf", "Download graph"),
+        downloadButton(style="display:inline", "downloadData_csv", "Download data")
+      ),
+      DTOutput("selectedGenes")
+    )
+  ),
+  fluidRow(style="padding-top:20px",
     column(12,
       DTOutput("expressionData")
     )
@@ -57,17 +65,20 @@ ui <- fluidPage(
 
 if (!exists("default_search_columns")) default_search_columns <- NULL
 
+# TODO: To add genotype checkbox functionality, just show/hide columns in lower dataframe, and
+# add if statements to plotting call. Much easier than changing allData/selectedData
+
 server <- function(input, output, session){
+
   RD <- reactiveValues(allData=RNAseq, selectedData=NULL)
   
-  output$expressionData <- renderDT(RD$allData[, c(1:3, 4, 6, 8, 10, 12, 14)], 
+  output$expressionData <- renderDT(RD$allData[, c(1:3, getGenotypeCols(input$selectedGenotypes))], 
     server=TRUE, filter="top", selection="none", class="compact hover",
-    colnames=c("Gene_ID", "Gene_name", "Gene_family", "WT_FPKM", "Ref5_FPKM", "Ref2_FPKM", "Med5_FPKM", "Ref5Med5_FPKM", "Ref2Med5_FPKM"),
     options=list(
       sDom="<'top'>rt<'bottom'>ip",
-      autoWidth=TRUE,
+      #autoWidth=TRUE,
       scrollX=TRUE,
-      columnDefs=list(list(width="7.5%", targets=c(4, 5, 6, 7, 8, 9)), list(width="15%", targets=c(1, 2))),
+      columnDefs=list(list(width="15%", targets=c(1, 2, 3))),
       pageLength=15,
       searchCols=default_search_columns,
       stateSave=FALSE)
@@ -85,20 +96,47 @@ server <- function(input, output, session){
       
       isolate({
         default_search_columns <- c("", input$expressionData_search_columns)
-        proxy %>% updateSearch(keywords=list(columns=default_search_columns))
+        proxy %>% updateSearch(keywords=list(columns=default_search_columns)) %>% reloadData(resetPaging=FALSE)
       })
     }
   })
 
+  output$downloadGraph_pdf <- downloadHandler(
+    filename=function(){paste("Arabidopsis_RNAseq", ".pdf", sep="")},
+    content=function(file){pdf(file, height=6, width=12)
+      plotData <- convertToTidy(RD$selectedData, input$selectedGenotypes)
+      plotData$Genotype <- factor(as.character(plotData$Genotype), levels=c("WT", "Ref5", "Ref2", "Med5", "Ref5Med5", "Ref2Med5"))
+      plotData$Gene_ID <- factor(as.character(plotData$Gene_ID), levels=RD$selectedData$Gene_ID)
+      print(ggplot(data=plotData, mapping=aes(x=Gene_ID, y=FPKM, fill=Genotype)) +
+              geom_col(position=position_dodge()) +
+              geom_errorbar(mapping=aes(ymin=FPKM-SE, ymax=FPKM+SE), color="black",
+                            width=0.2, position=position_dodge(0.9)) +
+              theme_bw() +
+              theme(axis.text=element_text(size=12),
+                    axis.title=element_text(size=14),
+                    legend.text=element_text(size=12),
+                    legend.title=element_text(size=14),
+                    legend.position="right")
+      )
+      dev.off()
+    }
+  )
+  
+  output$downloadData_csv <- downloadHandler(
+    filename=function(){paste("Arabidopsis_RNAseq", ".csv", sep="")},
+    content=function(file){
+      write.csv(RD$selectedData[, c(1:3, getGenotypeCols2(input$selectedGenotypes))], file, row.names=FALSE)
+    }
+  )
+  
   output$selectedGenes <- renderDT(RD$selectedData[, c(1, 2, 3)],
     server=TRUE, class="compact hover", selection="none",
-    colnames=c("Gene_ID", "Gene_name", "Gene_family"),
     options=list(
       ordering=FALSE,
       paging=FALSE,
       dom="rti",
       autoWidth=TRUE,
-      scrollY="320px",
+      scrollY="390px",
       searchCols=default_search_columns,
       stateSave=FALSE
     )
@@ -114,45 +152,26 @@ server <- function(input, output, session){
       
       isolate({
         default_search_columns <- c("", input$expressionData_search_columns)
-        proxy %>% updateSearch(keywords=list(columns=default_search_columns))
+        proxy %>% updateSearch(keywords=list(columns=default_search_columns)) %>% reloadData(resetPaging=FALSE)
       })
+      
+      if(nrow(RD$selectedData)==0){
+        RD$selectedData <- NULL
+      }
     }
   })
 
   output$genePlot <- renderPlot({
     if(!is.null(RD$selectedData)){
-      WT_data <- RD$selectedData[ , 1:5]
-      WT_data$Genotype <- "WT"
-      colnames(WT_data)[4:5] <- c("FPKM", "stdError")
       
-      Ref5_data <- RD$selectedData[ , c(1:3, 6:7)]
-      Ref5_data$Genotype <- "Ref5"
-      colnames(Ref5_data)[4:5] <- c("FPKM", "stdError")
+      plotData <- convertToTidy(RD$selectedData, input$selectedGenotypes)
       
-      Ref2_data <- RD$selectedData[ , c(1:3, 8:9)]
-      Ref2_data$Genotype <- "Ref2"
-      colnames(Ref2_data)[4:5] <- c("FPKM", "stdError")
-      
-      Med5_data <- RD$selectedData[ , c(1:3, 10:11)]
-      Med5_data$Genotype <- "Med5"
-      colnames(Med5_data)[4:5] <- c("FPKM", "stdError")
-      
-      Ref5Med5_data <- RD$selectedData[ , c(1:3, 12:13)]
-      Ref5Med5_data$Genotype <- "Ref5Med5"
-      colnames(Ref5Med5_data)[4:5] <- c("FPKM", "stdError")
-      
-      Ref2Med5_data <- RD$selectedData[ , c(1:3, 14:15)]
-      Ref2Med5_data$Genotype <- "Ref2Med5"
-      colnames(Ref2Med5_data)[4:5] <- c("FPKM", "stdError")
-      
-      plotData <- rbind(WT_data, Ref5_data, Ref2_data, Med5_data, Ref5Med5_data, Ref2Med5_data)
-      colnames(plotData)[1:3] <- c("Gene_ID", "Gene_name", "Gene_family")
       plotData$Genotype <- factor(as.character(plotData$Genotype), levels=c("WT", "Ref5", "Ref2", "Med5", "Ref5Med5", "Ref2Med5"))
-      plotData$Gene_ID <- factor(as.character(plotData$Gene_ID), levels=RD$selectedData$gene_id)
+      plotData$Gene_ID <- factor(as.character(plotData$Gene_ID), levels=RD$selectedData$Gene_ID)
       
       ggplot(data=plotData, mapping=aes(x=Gene_ID, y=FPKM, fill=Genotype)) +
         geom_col(position=position_dodge()) +
-        geom_errorbar(mapping=aes(ymin=FPKM-stdError, ymax=FPKM+stdError), color="black",
+        geom_errorbar(mapping=aes(ymin=FPKM-SE, ymax=FPKM+SE), color="black",
                       width=0.2, position=position_dodge(0.9)) +
         theme_bw() +
         theme(axis.text=element_text(size=12),
--- arabidopsis-RNASeq/helpers.R
+++ arabidopsis-RNASeq/helpers.R
@@ -1,2 +1,70 @@
 # Helper functions for main app
 
+# Function to create dataframe containing Gene_ID, Gene_name, Gene_family, mean FPKM, and se 
+# FKPM values for only selected genotypes from full RNAseq dataframe
+subsetGenotype <- function(data, selectedGenotypes){
+  subsetData <- data[, 1:3]
+  for(genotype in selectedGenotypes){
+    meanColName <- paste(genotype, "_FPKM", sep="")
+    meanCol <- data[, colnames(data)==meanColName]
+    seColName <- paste(genotype, "_FPKM_SE", sep="")
+    seCol <- data[, colnames(data)==seColName]
+    subsetData[, meanColName] <- meanCol
+    subsetData[, seColName] <- seCol
+  }
+  return(subsetData)
+}
+
+# Function to restore genotype data to selectedData (in case a genotype is deselected, then
+# reselected)
+restoreGenotype <- function(selectedData, allData, selectedGenotypes){
+  selectedRows <- rownames(selectedData)
+  selectedData <- NULL
+  rowCount <- 0
+  for(rowName in selectedRows){
+    rowCount <- rowCount + 1
+    selectedData <- rbind(selectedData, allData[rownames(allData)==rowName, ])
+    rownames(selectedData)[rowCount] <- rowName
+  }
+  return(selectedData)
+}
+
+# Function to convert data from wide format (with FPKM and SE values spread out across multiple
+# columns) to tidy format for plotting with ggplot
+convertToTidy <- function(data, selectedGenotypes){
+  tidyData <- NULL
+  for(genotype in selectedGenotypes){
+    genoData <- data[, 1:3]
+    meanColName <- paste(genotype, "_FPKM", sep="")
+    meanCol <- meanCol <- data[, colnames(data)==meanColName]
+    seColName <- paste(genotype, "_FPKM_SE", sep="")
+    seCol <- seCol <- data[, colnames(data)==seColName]
+    genoData$FPKM <- meanCol
+    genoData$SE <- seCol
+    genoData$Genotype <- genotype
+    tidyData <- rbind(tidyData, genoData)
+  }
+  return(tidyData)
+}
+
+getGenotypeCols <- function(selectedGenotypes){
+  genotypeCols <- vector(mode="numeric", length=0)
+  if(sum(selectedGenotypes=="WT")==1){genotypeCols <- append(genotypeCols, 4)}
+  if(sum(selectedGenotypes=="Ref5")==1){genotypeCols <- append(genotypeCols, 6)}
+  if(sum(selectedGenotypes=="Ref2")==1){genotypeCols <- append(genotypeCols, 8)}
+  if(sum(selectedGenotypes=="Med5")==1){genotypeCols <- append(genotypeCols, 10)}
+  if(sum(selectedGenotypes=="Ref5Med5")==1){genotypeCols <- append(genotypeCols, 12)}
+  if(sum(selectedGenotypes=="Ref2Med5")==1){genotypeCols <- append(genotypeCols, 14)}
+  return(genotypeCols)
+}
+
+getGenotypeCols2 <- function(selectedGenotypes){
+  genotypeCols <- vector(mode="numeric", length=0)
+  if(sum(selectedGenotypes=="WT")==1){genotypeCols <- append(genotypeCols, c(4, 5))}
+  if(sum(selectedGenotypes=="Ref5")==1){genotypeCols <- append(genotypeCols, c(6, 7))}
+  if(sum(selectedGenotypes=="Ref2")==1){genotypeCols <- append(genotypeCols, c(8, 9))}
+  if(sum(selectedGenotypes=="Med5")==1){genotypeCols <- append(genotypeCols, c(10, 11))}
+  if(sum(selectedGenotypes=="Ref5Med5")==1){genotypeCols <- append(genotypeCols, c(12, 13))}
+  if(sum(selectedGenotypes=="Ref2Med5")==1){genotypeCols <- append(genotypeCols, c(14, 15))}
+  return(genotypeCols)
+}
